a13aa4117841273eb728d31a64cd24ab14cd2707
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-06-07 20:13:09 +0300

    Handle button clicks

--- app/src/main/java/com/vetall24/myapplication/ui/numberconverter/NumberConverterFragment.kt
+++ app/src/main/java/com/vetall24/myapplication/ui/numberconverter/NumberConverterFragment.kt
@@ -2,18 +2,99 @@ package com.vetall24.myapplication.ui.numberconverter
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.vetall24.myapplication.R
+import com.vetall24.myapplication.databinding.FragmentNumberConverterBinding
 
-class NumberConverterFragment : Fragment() {
+class NumberConverterFragment : Fragment(R.layout.fragment_number_converter) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var binding: FragmentNumberConverterBinding
 
-        return inflater.inflate(R.layout.fragment_number_converter, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentNumberConverterBinding.bind(view)
+
+        handleClicks()
     }
 
+    private fun handleClicks() {
+        with(binding) {
+
+            btn0.setOnClickListener {
+                // TODO
+             }
+
+            btn1.setOnClickListener {
+                // TODO
+            }
+
+            btn2.setOnClickListener {
+                // TODO
+            }
+
+            btn3.setOnClickListener {
+                // TODO
+            }
+
+            btn4.setOnClickListener {
+                // TODO
+            }
+
+            btn5.setOnClickListener {
+                // TODO
+            }
+
+            btn6.setOnClickListener {
+                // TODO
+            }
+
+            btn7.setOnClickListener {
+                // TODO
+            }
+
+            btn8.setOnClickListener {
+                // TODO
+            }
+
+            btn9.setOnClickListener {
+                // TODO
+            }
+
+            btnDot.setOnClickListener {
+                // TODO
+            }
+
+            btnA.setOnClickListener {
+                // TODO
+            }
+
+            btnB.setOnClickListener {
+                // TODO
+            }
+
+            btnC.setOnClickListener {
+                // TODO
+            }
+
+            btnD.setOnClickListener {
+                // TODO
+            }
+
+            btnE.setOnClickListener {
+                // TODO
+            }
+
+            btnF.setOnClickListener {
+                // TODO
+            }
+
+            btnBackspace.setOnClickListener {
+                // TODO
+            }
+
+            btnAc.setOnClickListener {
+                // TODO
+            }
+        }
+    }
 }
