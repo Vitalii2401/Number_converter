e52ae7d279e42a90d2c6815dc12a6a9c3d390e6c
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-06-11 23:03:57 +0300

    Create use case for convert oct to bin

--- app/src/main/java/com/example/myapplication/di/UseCaseModule.kt
+++ app/src/main/java/com/example/myapplication/di/UseCaseModule.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.di
 import com.example.myapplication.domain.usecase.BinToDecUseCase
 import com.example.myapplication.domain.usecase.BinToHexUseCase
 import com.example.myapplication.domain.usecase.BinToOctUseCase
+import com.example.myapplication.domain.usecase.OctToBinUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
