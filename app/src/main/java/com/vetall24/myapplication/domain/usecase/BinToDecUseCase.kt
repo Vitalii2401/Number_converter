edf2f7f4264e5849973b96471881600969d78a6e
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-06-11 13:38:25 +0300

    Create use case for convert bin to dec

---
+++ app/src/main/java/com/vetall24/myapplication/domain/usecase/BinToDecUseCase.kt
@@ -0,0 +1,23 @@
+package com.vetall24.myapplication.domain.usecase
+
+import kotlin.math.pow
+
+class BinToDecUseCase {
+
+    fun execute(binValue: String): String {
+        return convertBinToDec(binValue)
+    }
+
+    private fun convertBinToDec(bin: String): String {
+        var decResult = 0
+        var index = 0
+        val size = bin.length - 1
+
+        for (it in bin) {
+            decResult += 2.0.pow(size - index).toInt() * it.digitToInt()
+            index++
+        }
+
+        return decResult.toString()
+    }
+}
