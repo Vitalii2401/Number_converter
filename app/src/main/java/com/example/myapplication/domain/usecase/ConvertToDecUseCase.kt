32678a758b71bc6a8d4908834a8d94dd2b4c29bc
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-12-13 18:38:55 +0200

    Changed convert to dec

--- app/src/main/java/com/example/myapplication/domain/usecase/ConvertToDecUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/ConvertToDecUseCase.kt
@@ -1,40 +1,27 @@
 package com.example.myapplication.domain.usecase
 
-import com.example.myapplication.utility.PRECISION
 import java.math.BigDecimal
 import java.math.MathContext
-import java.math.RoundingMode
 
 class ConvertToDecUseCase {
 
-    fun execute(value: String, numberSystem: Int): String =
-        if (value.contains(".")) {
-            val ratio = value.split(".")
-            (convertToDec(ratio[0], numberSystem, false).add(
-                convertToDec(ratio[1], numberSystem, true)))
-                .toString()
-        } else {
-            convertToDec(value, numberSystem, false).toString()
-        }
+    fun execute(value: String, fromNumberSystem: Int): String {
+        return convertToDec(value, fromNumberSystem).toString()
+    }
 
-
-    private fun convertToDec(value: String, numberSystem: Int, isValueAfterDot: Boolean): BigDecimal {
+    private fun convertToDec(inputValue: String, fromNumberSystem: Int): BigDecimal {
         var result = BigDecimal(0)
-        var index = if(isValueAfterDot) value.length else 0
-        val size = value.length - 1
-
-        var multiplyValue: BigDecimal
-        var addedValue: BigDecimal
-        var powerValue: Int
-
+        var powerValue =
+            if (inputValue.contains('.')) inputValue.indexOf('.') - 1 else inputValue.length - 1
+        val value = inputValue.replace(".", "")
 
         for (it in value) {
-            powerValue = size - index
-            multiplyValue = (it.toString().toInt(numberSystem)).toBigDecimal()
-            addedValue = numberSystem.toBigDecimal().pow(powerValue, MathContext.DECIMAL128).multiply(multiplyValue)
+            val multiplyValue = (it.toString().toInt(fromNumberSystem)).toBigDecimal()
+            val addedValue = fromNumberSystem.toBigDecimal().pow(powerValue, MathContext.DECIMAL128)
+                .multiply(multiplyValue)
 
             result = result.add(addedValue)
-            index++
+            powerValue--
         }
 
         return result
