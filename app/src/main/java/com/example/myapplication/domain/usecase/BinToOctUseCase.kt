1b1f277c8c3b63b967ec6331627dfef85885a96f
Vitalii2401 <movsesyan.vitaliy24ua@gmail.com>
2023-02-04 19:55:28 +0200

    Refactor use cases: deleted comments

--- app/src/main/java/com/example/myapplication/domain/usecase/BinToOctUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/BinToOctUseCase.kt
@@ -1,11 +1,9 @@
 package com.example.myapplication.domain.usecase
 
-import kotlin.math.pow
-
 class BinToOctUseCase(
     private val convertToDecUseCase: ConvertToDecUseCase,
     private val convertDecToUseCase: ConvertDecToUseCase
-)  {
+) {
 
     fun execute(binValue: String): String {
         return convertDecToUseCase.execute(
@@ -14,31 +12,4 @@ class BinToOctUseCase(
             256
         )
     }
-
-    /*private fun convertBinToOct(value: String): String {
-        var octResult = ""
-        var dec = convertBinToDec(value)
-
-        while (dec >= 8) {
-            octResult += (dec % 8).toString()
-            dec /= 8
-        }
-        octResult += dec.toString()
-        octResult = octResult.reversed()
-
-        return octResult
-    }
-
-    private fun convertBinToDec(bin: String): Int {
-        var dec = 0
-        var index = 0
-        val size = bin.length - 1
-
-        for (it in bin) {
-            dec += 2.0.pow(size - index).toInt() * it.digitToInt()
-            index++
-        }
-
-        return dec
-    }*/
 }
